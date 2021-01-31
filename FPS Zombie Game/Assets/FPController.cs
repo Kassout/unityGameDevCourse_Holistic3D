51e79ad0f115e21915b33e92fff67ee04fe3f5a1
mheliot <maxime.heliotpro@gmail.com>
2021-01-31 12:45:18 +0100

    A First Person Camera

--- FPS Zombie Game/Assets/FPController.cs
+++ FPS Zombie Game/Assets/FPController.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 
 public class FPController : MonoBehaviour
 {
+    public GameObject cam;
+
     [SerializeField] private float speed = 0.1f;
+    [SerializeField] private float xSensitivity = 2f;
+    [SerializeField] private float ySensitivity = 2f;
 
     private Rigidbody rb;
     private CapsuleCollider capsule;
+    private Quaternion cameraRotation;
+    private Quaternion characterRotation;
+    
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         capsule = GetComponent<CapsuleCollider>();
+
+        cameraRotation = cam.transform.localRotation;
+        characterRotation = transform.localRotation;
     }
 
     // Update is called once per frame
@@ -25,6 +35,16 @@ public class FPController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Rotating the character and the camera around X and Y axis
+        float yRotation = Input.GetAxis("Mouse X") * ySensitivity;
+        float xRotation = Input.GetAxis("Mouse Y") * xSensitivity;
+
+        cameraRotation *= Quaternion.Euler(-xRotation, 0, 0);
+        characterRotation *= Quaternion.Euler(0, yRotation, 0);
+
+        transform.localRotation = characterRotation;
+        cam.transform.localRotation = cameraRotation;
+        
         // GetKeyDown if you don't want the process to repeat until the player is pressing the key again
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
